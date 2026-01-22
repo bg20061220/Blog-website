0502dda54eb0fffd8b99b4cff25c41848e5a050b
bg20061220 <goel.bhavya020@gmail.com>
2026-01-21 19:39:23 -0500

    fix base path for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://djsiddz.github.io",
+  site: "https://bhavyagoel.netlify.app/",
   base: "/space-ahead",
   integrations: [
     swup({
