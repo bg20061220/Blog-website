987b05f3e1ce099a79e5018b60b03148b8438f5a
bg20061220 <goel.bhavya020@gmail.com>
2026-01-21 19:46:29 -0500

    Remove incorrect base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bhavyagoel.netlify.app/",
-  base: "/space-ahead",
+
   integrations: [
     swup({
       theme: ["overlay", { direction: "to-top" }],
